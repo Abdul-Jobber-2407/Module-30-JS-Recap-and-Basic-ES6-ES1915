7384c4b85148cac91b306a87d9ec313bbe920898
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2025-01-10 01:04:56 +0600

    arrow function done

---
+++ template.js
@@ -0,0 +1,36 @@
+const first = 'jaan' 
+const last = 'pakhi'
+const greet = 'potas potas'
+const name = first + ' ' +  last + " " + greet ;
+console.log(name);
+
+const a = 10 ;
+const b= 20 ;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + ( a+ b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
+
+const numbers = [45 , 48, 98, 78]
+const student= {name: 'john ochena', age:5}
+const total = `The sum of ${numbers[0]} and ${student.age} is ${numbers[0] + student.age}`
+console.log(total);
+
+const email = 'Hi john \n' + 
+'cena cena lage \n' + 
+'tumi mara mari korba naki';
+console.log(email)
+
+const challenge = `John Cena
+cena naki ochena
+mair dimu prochur`;
+console.log(challenge);
+
+//  \n --------> newline
+//  `` ------> Backtick
+//  ''----------> single quotes
+//  "" --------> double quotes 
+//   / --------> Forward slash
+//   \ -----> Backward slash 
+//   ; --------> semicolon
